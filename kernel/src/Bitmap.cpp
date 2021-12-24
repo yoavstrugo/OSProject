06a989bb228d7a7003dd0695dabd8e1c711f1aad
Yoav Strugo <yoav@strugo.net>
2021-12-24 16:57:26 +0200

    documented

--- kernel/src/Bitmap.cpp
+++ kernel/src/Bitmap.cpp
@@ -1,22 +1,28 @@
 #include "Bitmap.h"
 
 bool Bitmap::operator[](uint64_t index) {
+    // Calculate the specific bit and byte indices
     uint64_t byteIndex = index / 8;
     uint8_t bitIndex = index % 8;
+
+    // Get the bit to the correct index
     uint8_t bitIndexer = 0b10000000 >> bitIndex;
 
-    if ((Buffer[byteIndex] & bitIndexer) > 0) {
-        return true;
-    }
-    return false;
+    // Check the spceific bit in the specific byte
+    return (Buffer[byteIndex] & bitIndexer) > 0;
 }
 
 void Bitmap::Set(uint64_t index, bool value) {
+    // Calculate the specific bit and byte indices
     uint64_t byteIndex = index / 8;
     uint8_t bitIndex = index % 8;
+
+    // Get the bit to the correct index
     uint8_t bitIndexer = 0b10000000 >> bitIndex;
+
+    // Reset the bit
     Buffer[byteIndex] &= ~bitIndexer;
-    if (value) {
-        Buffer[byteIndex] |= bitIndexer;
-    }
+    
+    // Set the bit if neccessery
+    if (value) Buffer[byteIndex] |= bitIndexer;
 }
--- kernel/src/Bitmap.h
+++ kernel/src/Bitmap.h
@@ -2,10 +2,18 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/*
+* This class represents a bitmap.
+*
+* Since C++ is using byte-sized booleans, it can waste large amount of memory
+* to store boolean information on massive data (like pages).
+* Therefore we use a bitmap for storing the boolean value, where every single
+* bit repsents a boolean value.
+*/
 class Bitmap {
     public:
-    size_t Size;
-    uint8_t* Buffer;
-    bool operator[](uint64_t index);
-    void Set(uint64_t index, bool value);
+    size_t Size; // Size of the buffer
+    uint8_t* Buffer; // The buffer, an array of bytes
+    bool operator[](uint64_t index); // Get method (using operator overload, i.e Bitmap[2] - get the value at second index)
+    void Set(uint64_t index, bool value); // Set method
 };
