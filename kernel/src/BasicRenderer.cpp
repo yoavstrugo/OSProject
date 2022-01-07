08dda81053ec7c4a371af8499a2798a479e2453d
Yoav Strugo <yoav@strugo.net>
2022-01-07 08:59:53 -0800

    added printf

--- kernel/src/BasicRenderer.cpp
+++ kernel/src/BasicRenderer.cpp
@@ -4,7 +4,93 @@ BasicRenderer::BasicRenderer(FrameBuffer* targetFrameBuffer, PSF1_FONT* psf1_fon
     TargetFrameBuffer = targetFrameBuffer;
     PSF1_Font = psf1_font;
     Colour = 0xffffffff;
-    CursorPosition = {50, 50};
+    CursorPosition = {0, 0};
+}
+
+void BasicRenderer::AdvanceCursor() {
+    if (CursorPosition.X + PSF1_CHAR_WIDTH > TargetFrameBuffer->Width) 
+        NewLine();
+    else
+        CursorPosition.X += PSF1_CHAR_WIDTH;
+}
+
+void BasicRenderer::NewLine() {
+    CursorPosition = {0, CursorPosition.Y + LINE_HEIGHT};
+}
+
+bool BasicRenderer::strequ(const char *str1, const char *str2, bool match_length) {
+    if ((strlen(str1) != strlen(str2)) && match_length) return false;
+
+    for (int i = 0; i < MIN(strlen(str1), strlen(str2)); i++) {
+        if (str1[i] != str2[i]) return false;
+    }
+
+    return true;
+}
+
+void BasicRenderer::Printf(const char* format, ...) {
+    
+    int i;
+    char *s;
+
+    char *traverse = (char *)format;
+
+    va_list arg;
+    va_start(arg, format);
+
+    
+    for (char *traverse = (char *)format; *traverse != '\0'; traverse++) {
+        // Print all chracters until we get a '%'
+        while (*traverse != '%' && *traverse != '\0') {
+            PutChar(*traverse);
+            traverse++;
+        }
+
+        if (*traverse == '\0') break;
+
+        traverse++;
+
+        // After a '%', check the next character
+        switch (*traverse) {
+            case 'c':
+                i = va_arg(arg, int); // char argument
+                PutChar(i);
+                break;
+            case 'd':
+                i = va_arg(arg, int);
+                Print(toString((int64_t)i));
+                break;
+            case 's':
+                s = va_arg(arg, char *);
+                Print((const char *)s);
+                break;
+            case 'x':
+                i = va_arg(arg, uint64_t);
+                traverse++;
+                if (*traverse == '8')
+                    Print(toHexString((uint8_t)i));
+                else  if (strequ((const char *)traverse, "16", false)) {
+                    Print(toHexString((uint16_t)i));
+                    traverse++;
+                }
+                else  if (strequ((const char *)traverse, "32", false)) {
+                    Print(toHexString((uint32_t)i));
+                    traverse++;;
+                }
+                else  if (strequ((const char *)traverse, "64", false)) {
+                    Print(toHexString((uint64_t)i));
+                    traverse++;
+                }
+                else 
+                    traverse--;
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    va_end(arg);
 }
 
 void BasicRenderer::Print(const char* str) {
@@ -12,30 +98,31 @@ void BasicRenderer::Print(const char* str) {
     char* chr = (char*)str;
     while(*chr != 0) {
         // write the char and increase the cursor
-        PutChar(*chr, CursorPosition.X, CursorPosition.Y);
-        CursorPosition.X += 8;
-        // if the cursor reaches the end of the line move it down an reset the x offset
-        if (CursorPosition.X + 8 > TargetFrameBuffer->Width) {
-            CursorPosition.X = 0;
-            CursorPosition.Y += PSF1_Font->psf1_Header->charsize;
-        }
+        PutChar(*chr);
         chr++;
     }
 }
 
-void BasicRenderer::PutChar(char chr, unsigned int xOff, unsigned int yOff) {
+char BasicRenderer::PutChar(char chr) {
     // get the pointer to the graphics address
 	unsigned int* pixPtr = (unsigned int*) TargetFrameBuffer->BaseAddress;
     // get the pointer to the given char
 	char* fontPtr = (char*)PSF1_Font->glyphBuffer + (chr * PSF1_Font->psf1_Header->charsize);
+
+    if (chr == '\n') {
+        NewLine();
+        return chr;
+    }
+
+
     // loop on the vertical pixels
-	for (unsigned long y = yOff; y < yOff + 16; y++)
+	for (unsigned long y = CursorPosition.Y; y < CursorPosition.Y + 16; y++)
 	{
         // loop on the horizontal pixels
-		for (unsigned long x = xOff; x < xOff + 8; x++)
+		for (unsigned long x = CursorPosition.X; x < CursorPosition.X + 8; x++)
 		{
             // get the (x,y) pixel and check if it's on or off
-			if ((*fontPtr & (0b10000000 >> (x- xOff))) > 0) {
+			if ((*fontPtr & (0b10000000 >> (x- CursorPosition.X))) > 0) {
                 // if it's on write the pixel
 				*(unsigned int*)(pixPtr + x + (y * TargetFrameBuffer->PixelsPerScanline)) = Colour;
 			}
@@ -43,4 +130,8 @@ void BasicRenderer::PutChar(char chr, unsigned int xOff, unsigned int yOff) {
         // next row
 		fontPtr++;
 	}
+
+    AdvanceCursor(); // Set it ready for next character
+
+    return chr;
 }
--- kernel/src/BasicRenderer.h
+++ kernel/src/BasicRenderer.h
@@ -2,6 +2,11 @@
 #include "Math.h"
 #include "FrameBuffer.h"
 #include "PSF1.h"
+#include "cstr.h"
+#include <stdarg.h>
+#include <stddef.h>
+
+#define LINE_HEIGHT 16
 
 
 class BasicRenderer {
@@ -16,7 +21,11 @@ class BasicRenderer {
 
     // Class functions
     void Print(const char* str); // Print a string at the cursor position
+    void Printf(const char* str, ...);
+    void NewLine();
 
     private:
-    void PutChar(char chr, unsigned int xOff, unsigned int yOff); // Print a single char at the given position
+    char PutChar(char chr); // Print a single char at the given position
+    void AdvanceCursor(); 
+    bool strequ(const char *str1, const char *str2, bool match_length);
 };
