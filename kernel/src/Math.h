18fad04d471d92e312bd3dc7f98aa404ef8c7109
Yoav Strugo <yoav@strugo.net>
2022-01-07 09:01:34 -0800

    added min/max, toKB macros

--- kernel/src/Math.h
+++ kernel/src/Math.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) >(Y)) ? (X) : (Y))
+
+#define TO_KB(X) (X / 1024)
+
 // Represents a single point on screen
 struct Point {
 	unsigned int X;
